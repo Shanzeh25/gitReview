9ba1d5bb3d5e997149db8fccb607f8ca7a1db626
Shanzeh Asif <shanzehasif@Shanzehs-MacBook-Pro.local>
2022-06-01 06:36:02 +0500

    1st Commit

---
+++ src/gitreview/gitreview1.java
@@ -0,0 +1,12 @@
+package gitreview;
+
+public class gitreview1 {
+
+	public static void main(String[] args) {
+		System.out.print("today we learn github");
+		System.out.print("today is a very good dAY");
+		
+
+	}
+
+}
